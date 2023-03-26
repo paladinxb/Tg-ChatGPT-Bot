92b193cc865ec212f98e8df514cdb505ba5845b8
Илья <96050258+paladinxb@users.noreply.github.com>
2023-03-26 20:08:56 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import telebot
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import os
-os.environ["HUGGINGFACE_TOKEN"] = "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"
-model_name = "EleutherAI/gpt-j-6B"
+os.environ["HUGGINGFACE_TOKEN"] = "Your Token"
+model_name = "Whatever model name"
 # Загрузка модели и токенизатора ChatGPT
-openai_api_key = "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"
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", 
+openai_api_key = "Your openai API key"
+model = AutoModelForCausalLM.from_pretrained("model name", 
                                               from_tf=False, 
                                               gradient_checkpointing=False, 
                                               api_key=openai_api_key)
@@ -13,8 +13,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
 
 
 # Инициализация бота Telegram
-bot = telebot.TeleBot("5891690608:AAE6Mw5-M_HLkhXwSa1Cz_MYsy-OPXaFdos")
-
+bot = telebot.TeleBot("token of tg bot")
 # Обработчик входящих сообщений от пользователей
 @bot.message_handler(func=lambda message: True)
 
